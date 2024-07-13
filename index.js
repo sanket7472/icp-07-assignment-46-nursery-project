c3c77cafda1ea659a6a42851192156b88652a1c6
sanket7472 <sankettamnar2003@gmail.com>
2024-07-13 10:57:29 +0530

    CRUD Applied

--- index.js
+++ index.js
@@ -5,42 +5,113 @@ import express from "express"
 const app = express()
 app.use(express.json())
 
-const plants =[]
+const plants = []
 
-app.post( "/plant", (req,res)=>{
-    const 
-    {
-        name,
-         category,
-          image,
-           price,
+app.post("/plant", (req, res) => {
+    const
+        {
+            name,
+            category,
+            image,
+            price,
             description
-        } =req.body
- const randomid= math.round()(math.random()*10000)
- 
- const newplant=(
-    id = randomid,
-    name = name,
-    category = category,
-    image = image,
-    price = price,
-    description = description
+        } = req.body
+
+    if (!name) {
+        return res.json({
+            success: true,
+            data: null,
+            message: "Name is required"
+        })
+    }
+    const randomId = Math.round(Math.random() * 10000)
+
+    const newPlant = (
+        id = randomId,
+        name = name,
+        category = category,
+        image = image,
+        price = price,
+        description = description
 
     )
 
-    plants.push(newplant)
+    plants.push(newPlant)
 
     res.json({
         success: true,
-        data: newplant,
+        data: newPlant,
         message: "plant added successfully",
-      
+
     })
 
 })
+app.get("/plant", (req, res) => {
+    res.json({
+        success: true,
+        data: plants,
+        message: "plants fetched successfully",
+    })
+})
+app.get("/plant/:id", (req, res) => {
+    const { id } = req.params
+    const plant = plants.find((plant) => plant.id === id)
+    if (!plant) {
+        return res.json({
+            success: false,
+            data: null,
+            message: "plant not found",
+        })
+    }
+    res.json({
+        success: true,
+        data: plant,
+        message: "plant fetched successfully",
+    })
+})
+app.delete("/plant/:id", (req, res) => {
+    const { id } = req.params
+    const plantIndex = plants.findIndex((plant) => plant.id === id)
+    if (plantIndex === -1) {
+        return res.json({
+            success: false,
+            data: null,
+            message: "plant not found",
+        })
+    }
+    plants.splice(plantIndex, 1)
+    res.json({
+        success: true,
+        data: plants,
+        message: "plant deleted successfully",
+    })
+})
+app.put("/plant/:id", (req, res) => {
+    const { id } = req.params
+    const { name, category, image, price, description,
+    } = req.body
+    const plantIndex = plants.findIndex((plant) => plant.id === id)
+    if (plantIndex === -1) {
+        return res.json({
+            success: false,
+            data: null,
+            message: "plant not found",
+        })
+    }
+    plants[plantIndex] = {
+        ...plants[plantIndex], name, category, image,
+        price, description
+    }
+    res.json({
+        success: true,
+        data: plants,
+        message: "plant updated successfully",
+    })
+})
+
 
 const PORT = 5000
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
